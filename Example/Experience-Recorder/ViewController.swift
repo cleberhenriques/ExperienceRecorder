0ff4ef91db583492b14c431f5353704b00541c68
Henrique Morbin <morbin_@hotmail.com>
2016-01-18 12:06:23 -0200

    Pushing views with random background colors

--- Example/Experience-Recorder/ViewController.swift
+++ Example/Experience-Recorder/ViewController.swift
@@ -13,12 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = randomColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Methods
+    
+    func randomColor() -> UIColor{
+        return UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+        
+    }
 
+    // MARK: Actions
+
+    @IBAction func stopTouched(sender: AnyObject) {
+        
+    }
 }
 
